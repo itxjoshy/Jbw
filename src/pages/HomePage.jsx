3a1da9d7338215adc09b3b2f6a3232fef50e2017
itxjoshy <johnmanifest2017@gmail.com>
2026-08-06 23:28:00 +0100

    small ui change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -93,7 +93,7 @@ function HomePage() {
               search
             </span>
           </button>
-          <span className="nav-kids">Kids</span>
+          <span className="nav-kids">Josh</span>
           <button className="icon-button">
             <svg
               xmlns="http://www.w3.org/2000/svg"
